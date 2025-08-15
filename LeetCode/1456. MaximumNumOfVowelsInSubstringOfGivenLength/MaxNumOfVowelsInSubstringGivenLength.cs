94ba1cb807455aed567d4b3eabcb25afedb16606
Lia Moua <moua0061@yahoo.com>
2025-08-15 19:33:22 -0400

    added leet code 1456

--- LeetCode/1456. MaximumNumOfVowelsInSubstringOfGivenLength/MaxNumOfVowelsInSubstringGivenLength.cs
+++ LeetCode/1456. MaximumNumOfVowelsInSubstringOfGivenLength/MaxNumOfVowelsInSubstringGivenLength.cs
@@ -6,37 +6,55 @@ using System.Threading.Tasks;
 namespace LeetCode.1456. MaximumNumOfVowelsInSubstringOfGivenLength
 {
     public class MaxNumOfVowelsInSubstringGivenLength
-    {
-        public int MaxVowels(string s, int k) {
-            //keep track of the vowel counts:
-            int maxVowels = 0;
+{
+    public int MaxVowels(string s, int k) {
+        int maxVowels = 0; //keep track of the vowel counts:
+        int currentVcount = 0; //need a variable to count the current vowels in substring:
 
-            //need a tuple of vowels to loop thru
-            //char[] vowels = { 'a', 'e', 'i', 'o', 'u' }; 
-
-            //sliding window with k as the length:
-            //need a variable to count the current vowels in substring:
-            int currentVcount = 0;
-            //start at index 0
-            int start = 0;
-            //end at index k
-            int end = k;
-            //count the vowels in each substring within the string with k length
-            for (int i = start; end < s.Length; i++)
-            {
-                //Console.WriteLine($"What is current substring: {s[i]}");
-                int theVowel = s[i].ToLower();
-                if (theVowel == 'a' || theVowel == 'e' || theVowel == 'i' || theVowel == 'o' || theVowel == 'u')
-                {
-                    //if it's a vowel, increment the current count
-                    currentVcount++;
-                }
+        //add the count of the first window:
+        for (int i = 0; i < k; i++) {
+            char currentLetter = s[i];
+            if (isVowel(currentLetter)) { //if it is a vowel,
+                Console.WriteLine($"Found a vowel in first window: {currentLetter}");
+                currentVcount++; //increment the current vowel count
             }
-            //check if the current count is greater than the maxVowels
-            //set current count if greater than maxVowels
+        }
 
-            //return the maxVowels
-            return maxVowels;
+        //set maxVowels to the currentVcount:
+        maxVowels = currentVcount;
+
+        //count the vowels in each substring within the string with k length
+        //start where the first window left off
+        for (int i = k; k < s.Length; i++) {
+            char currentLetter = s[k];
+            if (isVowel(currentLetter)) //if the letter is a vowel,
+            {
+                Console.WriteLine($"Found a vowel!: {currentLetter}");
+                currentVcount++; //increment the current count
+            }
+
+            //if the letter leaving the window is a vowel,
+            char letterLeaving = s[i - k];
+            if (isVowel(currentLetter)) {
+                Console.WriteLine($"letter leaving window is a vowel!: {currentLetter}");
+                currentVcount--; //decrement the count
+            }
+
+            //check if the current count is greater than the maxVowels
+            maxVowels = Math.Max(maxVowels, currentVcount);
+        }
+
+        //return the maxVowels
+        return maxVowels;
+    }
+        
+        public bool isVowel(char letter){
+            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||){
+                return true;
+            }
+            
+            return false;
         }
     }
+    
 }
